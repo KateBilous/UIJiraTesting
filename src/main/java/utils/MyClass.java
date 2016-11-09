222b2662816d4d673718570267481e904d82d19f
KateBilous <katebilous@outlook.com>
2016-11-09 22:40:15 +0200

    GRID

--- src/main/java/utils/MyClass.java
+++ src/main/java/utils/MyClass.java
@@ -1,10 +1,13 @@
 package utils;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
 public class MyClass {
 
@@ -22,4 +25,17 @@ public class MyClass {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
     }
+    public void screen(WebDriver driver, String name){
+        File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        Random rn = new Random();
+        int randInt = rn.nextInt();
+        try {
+            FileUtils.copyFile(screen, new File("D:/Hillel/lab_hillel/LR_10/UITestingJira/"+name+randInt+".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
--- src/test/java/JiraUITests/JiraUI.java
+++ src/test/java/JiraUITests/JiraUI.java
@@ -1,41 +1,53 @@
 package JiraUITests;
 
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.LogoutPage;
 import pages.NewIssuePage;
 import pages.LoginPage;
 import pages.UpdateIssuePage;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
+import ru.yandex.qatools.allure.annotations.TestCaseId;
+import utils.MyClass;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class JiraUI {
     String issueKey = "";
-
+    WebDriver driver = maxWinTimeout();
 
 
 
     //@BeforeTest
     public void setUp(WebDriver driver) {
-        //WebDriver driver = tyt();
+
         LoginPage loginPage = new LoginPage(driver);
+        driver.get("http://soft.it-hillel.com.ua:8080/login.jsp");
         loginPage.enterUsername();
         loginPage.enterPassword();
         loginPage.clickLogin();
 
 
     }
-
+    @TestCaseId("Functions")
+    @Features("CreateIssue")
+    @Stories({"SomeStory"})
     @Test
     public void createIssue() {
-        WebDriver driver = tyt();
+
+        WebDriver driver = Grid();
+
         setUp(driver);
         NewIssuePage newIssuePage = new NewIssuePage(driver);
 
@@ -50,10 +62,13 @@ public class JiraUI {
 
     }
 
-
-    @Test
+    @TestCaseId("Functions")
+    @Features("CreateIssue")
+    @Stories({"SomeStory"})
+    @Test( groups ={"CreateIssueFunctions"})
     public void updateIssueAddComment() {
-        WebDriver driver = tyt();
+        WebDriver driver = Grid();
+
         setUp(driver);
         NewIssuePage newIssuePage = new NewIssuePage(driver);
         UpdateIssuePage updateIssuePage = new UpdateIssuePage(driver);
@@ -71,12 +86,14 @@ public class JiraUI {
         driver.quit();
 
 
-        // TODO assert
     }
-
-    @Test
+    @TestCaseId("Functions")
+    @Features("CreateIssue")
+    @Stories({"SomeStory"})
+    @Test( groups ={"CreateIssueFunctions"})
     public void updateReporter() {
-        WebDriver driver = tyt();
+
+        WebDriver driver = Grid();
         setUp(driver);
         NewIssuePage newIssuePage = new NewIssuePage(driver);
         UpdateIssuePage updateIssuePage = new UpdateIssuePage(driver);
@@ -94,12 +111,15 @@ public class JiraUI {
         driver.quit();
 
 
-        // TODO assert
-    }
 
-    @Test
+    }
+    @TestCaseId("Functions")
+    @Features("CreateIssue")
+    @Stories({"SomeStory"})
+    @Test( groups ={"CreateIssueFunctions"})
     public void updatePriority() {
-        WebDriver driver = tyt();
+        WebDriver driver = Grid();
+
         setUp(driver);
         NewIssuePage newIssuePage = new NewIssuePage(driver);
         UpdateIssuePage updateIssuePage = new UpdateIssuePage(driver);
@@ -117,12 +137,17 @@ public class JiraUI {
         driver.quit();
 
 
-        // TODO assert
+
     }
 
-    @Test
+
+
+    @TestCaseId("Functions")
+    @Features("CreateIssue")
+    @Stories({"SomeStory"})
+    @Test( groups ={"CreateIssueFunctions"})
     public void updateIssueTitle() {
-        WebDriver driver = tyt();
+        WebDriver driver = Grid();
         setUp(driver);
         NewIssuePage newIssuePage = new NewIssuePage(driver);
         UpdateIssuePage updateIssuePage = new UpdateIssuePage(driver);
@@ -138,34 +163,67 @@ public class JiraUI {
         updateIssuePage.deleteIssue();
         driver.quit();
 
-
-        // TODO assert
     }
-    public WebDriver tyt(){
+    public WebDriver maxWinTimeout(){
          WebDriver driver;
 
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS );
-        driver.get("http://soft.it-hillel.com.ua:8080/login.jsp");
+
         driver.manage().window().maximize();
         return driver;
     }
 
+    public WebDriver Grid(){
+        URL hostURL = null;
+        try {
+            hostURL = new URL("http://localhost:4444/wd/hub");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        capability.setBrowserName("chrome");
+        capability.setPlatform(Platform.LINUX);
 
-   // @AfterTest
-    public void tearDown() {
-        WebDriver driver = tyt();
-
-
-        driver.close(); // TODO investigate why browser is not closed
-        driver.quit();
+        WebDriver driver = new RemoteWebDriver(hostURL, capability);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://soft.it-hillel.com.ua:8080/login.jsp");
+        // разворачивает окно браузера
+        driver.manage().window().maximize();
+        return driver;
 
     }
+    @AfterMethod
+
+    public void afterMethod(ITestResult result){
+        MyClass myClass = new MyClass();
+        try{
+            if(result.getStatus() == ITestResult.SUCCESS){
+                //Do something here
+            }
+            else if(result.getStatus() == ITestResult.FAILURE){
+                //Do something here
+                myClass.screen(driver, "_FAILED_"+result.getName());
+            }
+            else if(result.getStatus() == ITestResult.SKIP ){
+                //Do something here
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    @AfterTest
+    public void afterTest(){
+        driver.quit();
+    }
+
+
 
 
     //Test(priority =2)
     public void Logout() {
-        WebDriver driver = tyt();
+        WebDriver driver = maxWinTimeout();
         LogoutPage logout = new LogoutPage(driver);
        logout.Logout();
 
